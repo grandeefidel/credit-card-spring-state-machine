e51a473660eb8d5c2fddbf8605d1edda43822486
grandeefidel <grandeefidel@gmail.com>
2024-09-02 15:41:20 +0200

    init commit

---
+++ src/main/java/com/fidelivery/ssm/creditcardssm/CreditCardSsmApplication.java
@@ -0,0 +1,13 @@
+package com.fidelivery.ssm.creditcardssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fidelivery/ssm/creditcardssm/CreditCardSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelivery.ssm.creditcardssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
